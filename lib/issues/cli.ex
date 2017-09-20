8869dcbd6f222063d753a9b9f8809177ecfb1e83
Katarzyna Kobierska <kkobierska@gmail.com>
2017-09-20 09:00:24 +0200

    Fix error message response

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -52,7 +52,7 @@ defmodule Issues.CLI do
   def decode_response({:ok, body}), do: Poison.decode!(body)
 
   def decode_response({:error, error}) do
-    {_, message} = List.keyfind(error, "message", 0)
+    message = Poison.decode!(error)["message"]
     IO.puts "Error fetching from Github: #{message}"
     System.halt(2)
   end
